17f711f67e6ef7fba1fd3ef24a76c60e57c3d7a6
Carlos Mena <50986837+carlosm00@users.noreply.github.com>
2022-09-09 12:46:41 +0200

    Updated mass-disk-exist

--- disks/mass-disk-exist.ps1
+++ disks/mass-disk-exist.ps1
@@ -6,14 +6,15 @@
 
     How to use:
 
-    snapshot-mass-remover.ps1 <subscription> <resource group> <file with disk names>
+    mass-disk-exist.ps1 <file with disk names>
+
+    NOTE: File should have subscription, resource group and disk name, separated by commas.
 #>
 
-$sub=$args[0]
-$rg=$args[1]
+# File is provided as the only argument
 $file = Get-Content -Path $args[0]
 
-if (Get-ChildItem --name disk_exists.csv 2>&1)
+if (Get-ChildItem --name 'disk_exists.csv' 2>&1)
 {
     Write-Host "Vertting positive results into disk_exists.csv"
 }
@@ -22,24 +23,28 @@ else {
     Write-Host "Vertting positive results into disk_exists.csv"
 }
 
-if (Get-ChildItem --name disk_dont_exists.csv 2>&1)
+if (Get-ChildItem --name 'disk_dont_exists.csv' 2>&1)
 {
-    Write-Host "...and disk_dont_exists.csv"
+    Write-Host "...and negative disk_dont_exists.csv"
 }
 else {
     New-Item -Path . -Name 'disk_dont_exists.csv' -ItemType "file"
-    Write-Host "...and disk_dont_exists.csv"
+    Write-Host "...and negative disk_dont_exists.csv"
 }
 
-az account set --subscription "$sub"
-
 foreach ($lines in $file){
-    if (az disk show --name "$lines" --resource-group "$rg" 2>&1){
-        Write-Host "$lines in $rg exists"
-        Write-Output "$sub,$rg,$lines" | Export-CSV disk_exists.csv
+
+    $obj = $lines.Split(',', 3)
+    $sub = $obj[0] 
+    $rg = $obj[1] 
+    $disk = $obj[2]
+
+    if (az disk show --subscription "$sub" --name "$disk" --resource-group "$rg" 2>&1){
+        Write-Host "$lines in $rg ($sub) exists"
+        Write-Output "$sub,$rg,$disk" | Export-CSV disk_exists.csv
     }
     else {
-        Write-Host "$lines in $rg does NOT exist"
-        Write-Output "$sub,$rg,$lines" | Export-CSV disk_dont_exists.csv
+        Write-Host "$disk in $rg ($sub) does NOT exist"
+        Write-Output "$sub,$rg,$disk" | Export-CSV disk_dont_exists.csv
     }
 }
